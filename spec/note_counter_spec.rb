4ff1bdc4572c1d567f79d619d621087253f7ca79
Joe Cannatti <joecannatti@gmail.com>
2013-02-25 15:24:50 -0500

    Test on real tab file

--- spec/note_counter_spec.rb
+++ spec/note_counter_spec.rb
@@ -53,6 +53,50 @@ describe NoteCounter do
       end
 
     end
+
+    context "two digit fret numbers" do
+
+      let(:two_digit_frets) do
+        "-3-\n-10-\n-0-\n-0-\n-2-\n-3-\nPANDA\n-3-\n   -0-\n-0-\n-0-\n-2-\n-3"
+      end
+
+      subject { described_class.new(two_digit_frets) }
+
+      it "reads the notes correctly" do
+        subject.notes[4][10].should eq 1
+      end
+
+    end
+
+    context "test tab" do
+    
+      let(:hendrix_tab) do
+        File.new(File.join(File.dirname(__FILE__), "hendrix.txt"), 'r').read
+      end
+
+      subject { described_class.new(hendrix_tab) }
+
+      it "reads the tab" do
+        subject.notes[0][12].should eq 1
+        subject.notes[0][3].should eq 2
+        subject.notes[0][0].should eq 1
+        subject.notes[1][0].should eq 1
+        subject.notes[1][2].should eq 1
+        subject.notes[1][5].should eq 1
+        subject.notes[1][7].should eq 1
+        subject.notes[2][12].should eq 1
+        subject.notes[2][5].should eq 3
+        subject.notes[2][7].should eq 1
+        subject.notes[3][0].should eq 3
+        subject.notes[3][5].should eq 5
+        subject.notes[3][4].should eq 3
+        subject.notes[4][12].should eq 1
+        subject.notes[4][3].should eq 6
+        subject.notes[5][12].should eq 1
+        subject.notes[5][5].should eq 1
+        subject.notes[5][3].should eq 3
+      end
+    end
     
   end
 
